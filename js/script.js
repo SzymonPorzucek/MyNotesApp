1f5a66b19bfd1daeb30c06660bd87a73b702467f
Szymon <szymoon.porzucek@gmail.com>
2024-05-09 15:04:44 +0200

    no mobile-final look

--- js/script.js
+++ js/script.js
@@ -10,11 +10,22 @@ const textarea = document.querySelector('#text');
 const category = document.querySelector('#category');
 const error = document.querySelector('.error');
 
+const deleteAskPopup = document.querySelector('.delete-all-popup');
+const deleteAllConfirmBtn = document.querySelector('.popup-confirm');
+const deleteAllCancelBtn = document.querySelector('.popup-cancel');
+
+const noteCounts = {
+	Shopping: 0,
+	Work: 0,
+	School: 0,
+	Home: 0,
+	Important: 0,
+	Other: 0,
+};
+
 let selectedValue;
 
 let cardID = 0;
-
-
 
 const openPanel = () => {
 	notePanel.style.display = 'flex';
@@ -27,10 +38,7 @@ const closePanel = () => {
 };
 
 const addNote = () => {
-	if (
-		textarea.value !== '' &&
-		category.options[category.selectedIndex].value !== '0'
-	) {
+	if (textarea.value !== '' && selectValue() !== '0') {
 		createNote();
 		error.style.visibility = 'hidden';
 	} else {
@@ -42,9 +50,11 @@ const createNote = () => {
 	newNote.classList.add('note');
 	newNote.setAttribute('id', cardID);
 
+	const categoryCount = ++noteCounts[selectedValue];
+
 	newNote.innerHTML = `
     <div class="note__header">
-    <h3 class="note-title">${selectedValue}</h3>
+    <h3 class="note-title">${selectedValue} #${categoryCount}</h3>
     <button class="delete-note" onclick="deleteNote(${cardID})">
         <i class="fas fa-times "></i>
     </button>
@@ -93,11 +103,37 @@ const deleteNote = id => {
 	noteArea.removeChild(noteToDelete);
 };
 
-const deleteAllNotes = () => {
+const deleteAll = () => {
+	const notes = noteArea.getElementsByClassName('note');
+	if (notes.length > 0) {
+		deleteAskPopup.style.display = 'block';
+		resetNoteCounts()
+	}
+};
+const deleteAllNotesConfirm = () => {
 	noteArea.textContent = '';
+	deleteAskPopup.style.display = 'none';
+};
+const showPopup = () => {
+	if (!(deleteAskPopup.style.display === 'block')) {
+		deleteAskPopup.style.display = 'block';
+	} else {
+		deleteAskPopup.style.display = 'none';
+	}
+};
+
+const resetNoteCounts = () => {
+	for (const category in noteCounts) {
+		noteCounts[category] = 0;
+	}
 };
 
 addBtn.addEventListener('click', openPanel);
 cancelBtn.addEventListener('click', closePanel);
 saveBtn.addEventListener('click', addNote);
-deleteAllBtn.addEventListener('click', deleteAllNotes);
+deleteAllBtn.addEventListener('click', deleteAll);
+deleteAllConfirmBtn.addEventListener('click', deleteAllNotesConfirm);
+deleteAllCancelBtn.addEventListener('click', showPopup);
+window.addEventListener('click', e =>
+	e.target === deleteAskPopup ? showPopup() : false
+);
